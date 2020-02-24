4ed4a2f18c5cd9b5b9092d1a708cafbdb006940f
Hannah Kofkin <hkofkin@gmail.com>
2020-02-24 19:07:39 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,9 +1,5 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(string = "Squawk!")
-  if parrot(string) == true
-    puts "Squawk!"
-  else
-    puts "Pretty bird!"
-  end
+  puts "Pretty bird!"
 end
